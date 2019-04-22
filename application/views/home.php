613ff121eedb348b2fff13ab5ced8ff88e096491
rizkyfjra <rizkyfajaranugrah2@gmail.com>
2019-04-22 21:08:06 +0700

    First Commit

---
+++ application/views/home.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <script src="<?php echo base_url('assets');?>/jquery/jquery.js"></script>
+    <script src="<?php echo base_url('assets');?>/bootstrap/js/bootstrap.js"></script>
+    <script src="<?php echo base_url('assets');?>/bootstrap/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets');?>/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets');?>/bootstrap/css/bootstrap-grid.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets');?>/bootstrap/css/bootstrap-reboot.css">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+  </ul>
+</nav>
+    <div class="row">
+    
+    </div>
+    <div class="row">
+    <div class="col-sm-4">
+    <img width="100%." height="240px" src="<?php echo base_url('assets');?>/images/konten.png">    
+    </div>
+        <div class="col-sm-4">
+    <img width="100%." height="240px" src="<?php echo base_url('assets');?>/images/konten.png">    
+    </div>
+        <div class="col-sm-4">
+    <img width="100%." height="240px" src="<?php echo base_url('assets');?>/images/konten.png">    
+    </div>
+    </div>
+
+</html>
